42f1bbaeeee443e634891d49dd1a746718cc341e
mjwhite10 <manublanco10037@gmail.com>
2022-06-10 04:35:13 +0200

    Añadiendo segmentacion de css, menu avatar, icono en boton ordenar

--- frontend/src/pages/MyDiverrsPage.jsx
+++ frontend/src/pages/MyDiverrsPage.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from 'react';
 
 const MyDiverrsPage = ({ hideItems }) => {
-  useEffect(() => {
-    hideItems(true);
-  });
+
   return <p>Welcome to my diverrs page</p>;
 };
 export default MyDiverrsPage;
