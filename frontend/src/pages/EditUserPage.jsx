06a951dfd439c2f4efda41d30d1fb4d0d733b027
Luna Cadima <lunakadima@yahoo.es>
2022-06-11 20:50:15 +0200

    Modificando EditUserPage

--- frontend/src/pages/EditUserPage.jsx
+++ frontend/src/pages/EditUserPage.jsx
@@ -1,4 +1,40 @@
 const EditUserPage = () => {
-  return <p>Welcome to edit user page</p>;
+  return (
+    <section className="editUser-page">
+      <form className="editUser-form">
+        <h1>Editar usuario</h1>
+        <label htmlFor="nombre">Nombre</label>
+        <editUserForm
+          className="inputField"
+          id={"nombre"}
+          placeholder={""}
+          type={"text"}
+        />
+        <label htmlFor="apellido">Apellido</label>
+        <editUserForm
+          className="inputField"
+          id={"apellido"}
+          placeholder={""}
+          type={"text"}
+        />
+        <label htmlFor="edad">Edad</label>
+        <editUserForm
+          className="inputField"
+          id={"edad"}
+          placeholder={""}
+          type={"number"}
+        />
+        <label htmlFor="sector">Sector</label>
+        <editUserForm
+          className="inputField"
+          id={"sector"}
+          placeholder={""}
+          type={"text"}
+        />
+        <button type='submit'>Confirmar cambios</button>
+      </form>
+    </section>
+  );
 };
+
 export default EditUserPage;
