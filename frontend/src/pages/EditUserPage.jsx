39479e903c82511b0c7ba9f0b81b7d7eef1fdb4b
Luna Cadima <lunakadima@yahoo.es>
2022-06-13 13:26:21 +0200

    Añadiendo css a edit user

--- frontend/src/pages/EditUserPage.jsx
+++ frontend/src/pages/EditUserPage.jsx
@@ -2,38 +2,38 @@ import LoadAvatar from "../components/LoadAvatar";
 
 const EditUserPage = () => {
   return (
-    <section className="login-page">
-      <form className="login-form">
+    <section className="form-page">
+      <form className="form">
         <h1>Editar usuario</h1>
         <label htmlFor="nombre">Nombre</label>
         <input
-          className="inputField"
+          className="input-field-form"
           id={"nombre"}
           placeholder={""}
           type={"text"}
         />
         <label htmlFor="apellido">Apellido</label>
         <input
-          className="inputField"
+          className="input-field-form"
           id={"apellido"}
           placeholder={""}
           type={"text"}
         />
         <label htmlFor="edad">Edad</label>
         <input
-          className="inputField"
+          className="input-field-form"
           id={"edad"}
           placeholder={""}
           type={"number"}
         />
         <label htmlFor="sector">Sector</label>
         <input
-          className="inputField"
+          className="input-field-form"
           id={"sector"}
           placeholder={""}
           type={"text"}
         />
-        <button className="login-button primary-button" type='submit'>Confirmar cambios</button>
+        <button className="form-button primary-button" type='submit'>Confirmar cambios</button>
       </form>
       <LoadAvatar />
     </section>
