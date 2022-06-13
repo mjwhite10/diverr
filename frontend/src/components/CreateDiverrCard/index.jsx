d966e0f8067adf3f0404ad84f86c4032a47f73ac
Luna Cadima <lunakadima@yahoo.es>
2022-06-13 19:30:08 +0200

    Creando componente Create Diverr Card

---
+++ frontend/src/components/CreateDiverrCard/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const CreateDiverrCard = () => {
+    return (
+      <article className="create-diverr-card">
+        <p>+</p>
+        <p>Crear diver</p>
+      </article>
+    );
+  };
+  export default CreateDiverrCard;
