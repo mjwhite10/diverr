4b67c5fe878ef8ca0b9559933b2160fe38a18c19
mjwhite10 <manublanco10037@gmail.com>
2022-06-15 13:28:26 +0200

    Modificando edituser page

--- frontend/src/pages/MyDiverrsPage.jsx
+++ frontend/src/pages/MyDiverrsPage.jsx
@@ -1,18 +1,10 @@
+import CreateDiverrCard from '../components/CreateDiverrCard';
+
 const MyDiverrsPage = () => {
   return (
-    <div className="mydiverrs-container">
-      <div className="mydiverrs-container-slider">
-        <img src="https://via.placeholder.com/210?text=1" alt="" />
-        <img src="https://via.placeholder.com/220?text=2" alt="" />
-        <img src="https://via.placeholder.com/230?text=3" alt="" />
-        <img src="https://via.placeholder.com/240?text=4" alt="" />
-        <img src="https://via.placeholder.com/250?text=5" alt="" />
-        <img src="https://via.placeholder.com/260?text=6" alt="" />
-        <img src="https://via.placeholder.com/270?text=7" alt="" />
-        <img src="https://via.placeholder.com/280?text=8" alt="" />
-        <img src="https://via.placeholder.com/290?text=9" alt="" />
-      </div>
-    </div>
+    <section className="mydiverrs-container">
+      <CreateDiverrCard />
+    </section>
   );
 };
 export default MyDiverrsPage;
