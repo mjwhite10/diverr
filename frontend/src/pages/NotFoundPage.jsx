ae6bce80af1363a7651a978c13dd71ace0ee37fa
mjwhite10 <manublanco10037@gmail.com>
2022-06-19 22:03:55 +0200

    Modificando diver-page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <section className='error-page'>
-      <h1>Page not found</h1>
-      <img  className='error-page-gif' src="../images/error.gif" alt="error cat" />
-      <Link to={'/'}>Go to Home Page</Link>
+    <section className="error-page">
+      <h1>La página no existe</h1>
+      <img
+        className="error-page-gif"
+        src="../images/error.gif"
+        alt="error cat"
+      />
+      <Link className="link" to={'/'}>
+        Descuida, Gatete te llevará a la Home Page
+      </Link>
     </section>
   );
 };
